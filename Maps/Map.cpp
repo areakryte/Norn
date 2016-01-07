9dabfe3bdf2cbfcdc21299aebb725142d4393598
areakryte <ydavidf@gmail.com>
2016-01-07 11:37:48 -0800

    More comments.

--- Maps/Map.cpp
+++ Maps/Map.cpp
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The Map class is the bases for all in field movement and combat.
  */
 
 /* 
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Norn
 Crappy Game Engine built to review C++ and other concepts.
 Project needs tinkering.
+# TODO:
+    - Menus
+    - Event Handling
+    - Coding in general
+    - Brainstorming in general
